4f2589017dc8dde9d90fb7432049218028795b93
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-05-20 16:48:40 +0200

    put data from database in cards

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,1 +1,20 @@
 @extends('layouts.base_layout')
+
+@section('homepage')
+    <div class="contaner-fluid px-5">
+        <div class="row flex-wrap justify-content-center">
+            @foreach ($movies as $movie)
+                <div class="col-5 movieCard text-center">
+                    <h1>{{ $movie['title'] }}</h1>
+                    <div class="originalTitle">
+                        <h5>Original Title: </h5>
+                        <h2>{{ $movie['original_title'] }}</h2>
+                    </div>
+                    <p>Nationality: {{ $movie['nationality'] }}</p>
+                    <h6>Release Date: {{ $movie['date'] }}</h6>
+                    <h6>Vote: {{ $movie['vote'] }}/10</h6>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
